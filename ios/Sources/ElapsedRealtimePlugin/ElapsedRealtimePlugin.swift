6ca169fd279f50ed5d367d3749a89c5d7b0e73e3
Tuelho <tuelhoo@gmail.com>
2024-10-15 09:58:51 -0300

    getElapsedRealtime

--- ios/Sources/ElapsedRealtimePlugin/ElapsedRealtimePlugin.swift
+++ ios/Sources/ElapsedRealtimePlugin/ElapsedRealtimePlugin.swift
@@ -10,16 +10,9 @@ public class ElapsedRealtimePlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "ElapsedRealtimePlugin"
     public let jsName = "ElapsedRealtime"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "getElapsedRealtime", returnType: CAPPluginReturnPromise)
     ]
     private let implementation = ElapsedRealtime()
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
-    }
 
     @objc func getElapsedRealtime(_ call: CAPPluginCall) {
         let elapsedTime = ProcessInfo.processInfo.systemUptime * 1000 // Convert to milliseconds
